5d4d976fb6bb20a36e1bf7cc55c7f580d437cc5a
wgharbieh <wgharbieh@hotmail.com>
2015-12-01 16:57:45 -0400

    This is the end of part 7 in activity 3

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello your-nick-name!");
+		System.out.println("Hello your-full-name!");
 	}
 
 }
